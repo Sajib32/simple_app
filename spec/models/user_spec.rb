bf165842c1b1513868c088b6d053564805220d58
Sajib32 <sajibcste@gmail.com>
2014-07-28 18:35:32 +0000

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,17 +13,18 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
-    describe "when password is not present" do
-  before do
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: " ", password_confirmation: " ")
-  end
-  it { should_not be_valid }
-end
+   describe "when password is not present" do
+    before do
+        @user = User.new(name: "Example User", email: "user@example.com",
+                         password: " ", password_confirmation: " ")
+        end
+      it { should_not be_valid }
+   end
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
@@ -88,4 +89,8 @@ describe "with a password that's too short" do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end 
+  describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
+   end
 end
